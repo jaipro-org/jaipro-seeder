cfc5a6c5c1000e6b92ee8dc2356832802787f3fc
Panda <63072537+giovanny110@users.noreply.github.com>
2023-08-19 04:11:43 -0500

    HEP-95: update files: 'R__get_list_service_request_by_specialist' and 'R__get_total_rows_list_service_request_by_specialist', and add file 'R__get_detail_service_proposal_by_id'

--- src/main/resources/db/migration/R__get_total_rows_list_service_request_by_specialist.sql
+++ src/main/resources/db/migration/R__get_total_rows_list_service_request_by_specialist.sql
@@ -5,15 +5,14 @@ AS
 $function$
 begin
     return (select count(*)
-            from (select sr.service_request_id
-                  from specialist_specialization ss
-                           join service_request sr on ss.profession_id = sr.profession_id
-                           join customer c on sr.customer_id = c.customer_id
-                           join profession p on sr.profession_id = p.profession_id
-                  where ss.specialist_id = p_specialist_id
-                    and (p_status = '' or sr.status = any (cast(p_status as int[])))
-                  group by sr.service_request_id, c."name", c.last_name, p."name", sr.detail, sr.creation_date,
-                           sr.status) as t);
+            from (select sp.service_proposal_id
+                  from service_request sr
+                      join service_proposal sp on sr.service_request_id = sp.service_request_id
+                      join customer c on sr.customer_id = c.customer_id
+                      join profession p on sr.profession_id = p.profession_id
+                  where sp.specialist_id = p_specialist_id
+                  and (p_status = '' or sp.status_proposal = any (cast(p_status as int[])))
+                  ) as t);
 end
 $function$
 ;
