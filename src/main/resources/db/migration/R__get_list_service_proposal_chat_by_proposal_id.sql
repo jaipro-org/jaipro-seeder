e117e298ba421e65b1950101c185a8d5d14169c7
Panda <63072537+giovanny110@users.noreply.github.com>
2023-10-21 17:54:45 -0500

    HEP-215: change parameters in function get_list_service_proposal_chat_by_proposal_id

--- src/main/resources/db/migration/R__get_list_service_proposal_chat_by_proposal_id.sql
+++ src/main/resources/db/migration/R__get_list_service_proposal_chat_by_proposal_id.sql
@@ -1,18 +1,18 @@
-CREATE OR REPLACE FUNCTION jaipro.get_list_service_proposal_chat_by_proposal_id(p_service_proposal_id uuid, p_page integer, p_page_size integer)
+CREATE OR REPLACE FUNCTION jaipro.get_list_service_proposal_chat_by_proposal_id(p_service_proposal_id uuid, p_total_messages_showed integer, p_page_size integer)
  RETURNS TABLE(message character varying, creation_date timestamp without time zone, from_specialist boolean, from_customer boolean, rows integer)
  LANGUAGE plpgsql
 AS $function$
 begin
-    return query (select
+    return query (select 
 						spc.message,
 						spc.creation_date,
 						spc.from_specialist,
 						spc.from_customer,
 						CAST(count(*) over() as int) as rows
-				  from service_proposal_chat spc
+				  from service_proposal_chat spc 
 				  where spc.service_proposal_id = p_service_proposal_id
                   order by spc.creation_date desc
-                  limit p_page_size offset ((p_page - 1) * p_page_size));
+                  limit p_page_size offset (p_total_messages_showed));
 end
 $function$
 ;
